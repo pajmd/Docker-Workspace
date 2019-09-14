81f53899131c5dadc5da783751f8873e70e3c9a1
pajmd <philippejmd@gmail.com>
2019-09-14 15:55:51 +0200

    added script to start or stop the full app with clean db or not

---
+++ nhs-app-full-stack/nhs-full-stack.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# start stop nhs-full-stack
+
+
+usage() {
+	cat << EOF
+To start or stop nhs full stack:
+
+usage: $0 [start [-c|--clean]|stop|suspend]
+
+EOF
+	exit 0
+}
+
+if [ -z $1 ]; then
+	usage
+elif [[ $1 = "start" ]]; then
+	if [[ -z $2 ]]; then
+		echo "start"
+		docker-compose up -d
+	elif [[ $2 =~ ^-c(lean)?$ ]]; then
+		echo "start clean"
+		sudo -u mongodb rm -rf $HOME/tmp/fullstack/data/db/* && docker-compose up -d
+	else
+		echo "$2 option unknown"
+		usage
+	fi
+elif [[ $1 = "stop" ]]; then
+	echo "stop"
+	docker-compose down
+elif [[ $1 = "suspend" ]]; then
+	echo "suspend"
+	docker-compose stop
+else
+	echo "$1 unknown command"
+fi
